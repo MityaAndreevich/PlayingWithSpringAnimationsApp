426aaac9c7eaf54d129ba74da5179cc5958a412f
MityaAndreevich <dmitry.logachev@icloud.com>
2021-09-22 16:16:23 +0300

    first spring animation added

--- PlayingWithSpringAnimationsApp/ViewController.swift
+++ PlayingWithSpringAnimationsApp/ViewController.swift
@@ -5,15 +5,24 @@
 //  Created by Dmitry Logachev on 22.09.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var springAnimationView: SpringView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
-
+    @IBAction func runSpringAnimation(_ sender: SpringButton) {
+        springAnimationView.animation = "morph"
+        springAnimationView.curve = "easeIn"
+        springAnimationView.animate()
+        
+        sender.animation = "wobble"
+        sender.animate()
+    }
+    
 }
 
