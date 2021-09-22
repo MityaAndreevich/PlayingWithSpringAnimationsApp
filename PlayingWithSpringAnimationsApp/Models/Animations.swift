749272ad135abfc2c953f3e2f36b53b3e5ee2260
MityaAndreevich <dmitry.logachev@icloud.com>
2021-09-22 22:10:04 +0300

    added first method, needs improvements to be changed every time

--- PlayingWithSpringAnimationsApp/Models/Animations.swift
+++ PlayingWithSpringAnimationsApp/Models/Animations.swift
@@ -5,29 +5,17 @@
 //  Created by Dmitry Logachev on 22.09.2021.
 //
 
-import Foundation
-
-struct Animations {
+struct Animation {
     let animation: String
     let curve: String
 }
 
-extension Animations {
-    static func getAnimationName() -> [Animations] {
-        var allAnimations: [Animations] = []
-        
-        let animationNames = DataManager.shared.animationNames.shuffled()
-        let curveNames = DataManager.shared.curveNames.shuffled()
-        
-        let iterationCount = min(animationNames.count, curveNames.count)
-        
-        for index in 0..<iterationCount {
-            let animation = Animations(
-                animation: animationNames[index],
-                curve: curveNames[index]
-            )
-            allAnimations.append(animation)
-        }
-        return allAnimations
+extension Animation {
+    
+    static func getAnimation() -> Animation {
+        Animation(
+            animation: DataManager.shared.animationNames.randomElement() ?? "",
+            curve: DataManager.shared.curveNames.randomElement() ?? ""
+        )
     }
 }
--- PlayingWithSpringAnimationsApp/ViewControllers/ViewController.swift
+++ PlayingWithSpringAnimationsApp/ViewControllers/ViewController.swift
@@ -8,21 +8,42 @@
 import Spring
 
 class ViewController: UIViewController {
+    
+    private var allAnimations = Animation.getAnimation()
+    
+    
     @IBOutlet weak var springAnimationView: SpringView!
+    
+    @IBOutlet weak var animationNameLabel: UILabel!
+    @IBOutlet weak var animationCurveLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
+        animationNameLabel.text = "Here will appear animation name"
+        animationCurveLabel.text = "Here will appear curve name"
     }
 
     @IBAction func runSpringAnimation(_ sender: SpringButton) {
-        springAnimationView.animation = "morph"
-        springAnimationView.curve = "easeIn"
-        springAnimationView.animate()
         
-        sender.animation = "wobble"
-        sender.animate()
+        setAnimation()
+        setLabelsDescription()
+        
+        //springAnimationView.animation = allAnimations.animation
+        //springAnimationView.curve = allAnimations.curve
+       // springAnimationView.animate()
+        
     }
     
+    private func setAnimation() {
+        springAnimationView.animation = allAnimations.animation
+        springAnimationView.curve = allAnimations.curve
+        springAnimationView.animate()
+    }
+    
+    private func setLabelsDescription() {
+        animationNameLabel.text = allAnimations.animation
+        animationCurveLabel.text = allAnimations.curve
+    }
 }
 
