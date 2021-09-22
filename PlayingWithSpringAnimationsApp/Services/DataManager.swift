adbd9bf7e1e41a90bb9952f0df6578e12537acb9
MityaAndreevich <dmitry.logachev@icloud.com>
2021-09-22 20:07:58 +0300

    model and data manager added

---
+++ PlayingWithSpringAnimationsApp/Services/DataManager.swift
@@ -0,0 +1,53 @@
+//
+//  DataManager.swift
+//  PlayingWithSpringAnimationsApp
+//
+//  Created by Dmitry Logachev on 22.09.2021.
+//
+
+import Foundation
+
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    let animationNames = [
+        "shake",
+        "pop",
+        "morph",
+        "squeeze",
+        "wobble",
+        "swing",
+        "flipX",
+        "flipY",
+        "fall",
+        "squeezeLeft",
+        "squeezeRight",
+        "squeezeDown",
+        "squeezeUp",
+        "slideLeft",
+        "slideRight",
+        "slideDown",
+        "slideUp",
+        "fadeIn",
+        "fadeOut",
+        "fadeInLeft",
+        "fadeInRight",
+        "fadeInDown",
+        "fadeInUp",
+        "zoomIn",
+        "zoomOut",
+        "flash"
+    ]
+    
+    let curveNames = [
+        "spring",
+        "linear",
+        "easeIn",
+        "easeOut",
+        "easeInOut"
+    ]
+    
+    private init() {}
+    
+}
