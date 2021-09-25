df2f2d02464544b15da6b52e120b3935b6162fe3
MityaAndreevich <dmitry.logachev@icloud.com>
2021-09-25 15:46:36 +0300

    added more info for animation (force, duration, delay)

--- PlayingWithSpringAnimationsApp/Models/Animations.swift
+++ PlayingWithSpringAnimationsApp/Models/Animations.swift
@@ -10,11 +10,17 @@ import Foundation
 struct Animation {
     let animation: String
     let curve: String
+    let force: Float
+    let duration: Float
+    let delay: Float
     
     var desciption: String {
         """
         preset: \(animation)
         curve: \(curve)
+        force: \(String(format: "%.02f", force))
+        duration: \(String(format: "%.02f", duration))
+        delay: \(String(format: "%0.02f", delay))
         """
     }
 }
@@ -24,7 +30,10 @@ extension Animation {
     static func getAnimation() -> Animation {
         Animation(
             animation: DataManager.shared.animationNames.randomElement() ?? "",
-            curve: DataManager.shared.curveNames.randomElement() ?? ""
+            curve: DataManager.shared.curveNames.randomElement() ?? "",
+            force: Float.random(in: 1...2),
+            duration: Float.random(in: 1...2),
+            delay: 0.3
         )
     }
 }
